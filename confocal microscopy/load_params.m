4f7911e550be23ccc4d703e7fe522c623a410b57
Tim Weber <tweber225@gmail.com>
2020-09-16 20:11:06 -0400

    added a tiff saving utility

--- confocal microscopy/load_params.m
+++ confocal microscopy/load_params.m
@@ -5,11 +5,11 @@ p.zRange = 30;      % microns, plus or minus from in-focus
 
 
 % Illumination
-p.NAi = 0.12;        % dimensionless
+p.NAi = 0.24;        % dimensionless
 p.wli = 0.561;      % microns
 
 % Detection
-p.NAd = 0.5;            % dimensionless
+p.NAd = 0.95;            % dimensionless
 p.wld = 0.610;          % microns
 p.pinholeDiam = 2.88;   % microns in sample, don't do exactly 0 for small pinhole
 
