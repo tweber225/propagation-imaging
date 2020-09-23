d726a1ea812f7596f7abcfd858086bf7be79467c
Tim Weber <tweber225@gmail.com>
2020-09-23 08:58:28 -0400

    Update load_params.m

--- confocal microscopy/load_params.m
+++ confocal microscopy/load_params.m
@@ -1,7 +1,7 @@
 % Parameters
 p.sampDens = 0.1;   % microns
 p.numPoints = 128;  % number
-p.zRange = 10;      % microns, plus or minus from in-focus
+p.zRange = 18;      % microns, plus or minus from in-focus
 p.n = 1.333;        % refractive index of object medium
 
 
@@ -9,14 +9,14 @@ p.n = 1.333;        % refractive index of object medium
 p.NAi = 0.95;           % limiting aperture for illumination
 p.wli = 0.561;          % microns
 p.zOffseti = 0;         % microns axial displacement
-p.waisti = 1;          % fraction of limiting aperture, inf to make beam flat-top
+p.waisti = .18;          % fraction of limiting aperture, inf to make beam flat-top
 
 % Detection
 p.NAd = 0.95;            % dimensionless
 p.wld = 0.610;          % microns
 p.pinholeDiam = 1.31;   % microns in sample, don't do exactly 0 for small pinhole
-p.zOffsetd = 0;         % microns axial displacement
-p.upPin = false;
+p.zOffsetd = 5.97;         % microns axial displacement
+p.upPin = true;
 p.upPinOffset = 5.97;    % microns
 
 
