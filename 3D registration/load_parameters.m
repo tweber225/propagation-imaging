fa2a293286590c64ac7de83c7d311580a5df34fb
Tim Weber <tweber225@gmail.com>
2021-02-09 11:34:58 -0500

    made new stack

--- 3D registration/load_parameters.m
+++ 3D registration/load_parameters.m
@@ -1,6 +1,6 @@
 % User Parameters
 p.pixSize = 0.5;    % microns
-p.numLatPix = 128;       % number pixels per lateral dimension
+p.numLatPix = 256;       % number pixels per lateral dimension
 p.zPixSize = p.pixSize; % for simplicity keep the same z spacing size as lateral pixels
 p.numZPix = 128;
 p.n = 1;                % refractive index of object medium
@@ -8,19 +8,19 @@ p.NA = 0.4;            % dimensionless
 p.NAi = 0.4;
 p.wl = 0.850;          % microns
 p.focalPlanes = -15:5:20;   %microns
-p.noiseLevel = 1.81*2;
+p.noiseLevel = 1.81;
 p.upSampFact = 1; % For sub-voxel registration
 p.filterThreshold = 10;
 
 % Object Parameters
-p.obj.particleCount = 256;
+p.obj.particleCount = 512;
 p.obj.r = 1.51;
 p.obj.dn = 0.01; % This doesn't really mean anything in terms of absolute refractive index
 p.obj.timeStart = 0;
 p.obj.timeEnd = 1;
 p.obj.numStacks = 64;
-p.obj.velocity = [0.143; -12.21; -10.21]; % Constant added velocity
-p.obj.movementAmplitude = [-7.44; 0; 6];
+p.obj.velocity = [0.743; -22.21; -10.21]; % Constant added velocity
+p.obj.movementAmplitude = [-9.44; 0; 6];
 p.obj.movementPeriod = 1;
 p.obj.numTimesPerFrame = 4; % Approximation of intra-frame blurring
 
