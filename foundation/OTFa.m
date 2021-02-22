c17d439fd38f052b004dc50a2e8d81a69829dc35
Tim Weber <tweber225@gmail.com>
2021-02-22 10:59:49 -0500

    Update OTFa.m

--- foundation/OTFa.m
+++ foundation/OTFa.m
@@ -1,14 +1,14 @@
-function out = OTFp(p,d,symFlag)
+function out = OTFa(sfCutoff,sfCutoffi,k,zPosIdx,sfIdx,symFlag)
 
 % Compute a few factors
-H_det = H(d.sfIdx,p.sfCutoff);
+H_det = H(sfIdx,sfCutoff);
 if strcmp(symFlag,'asym')
-    H_illum = abs(H_asym(-d.sfIdx,p.sfCutoffi)).^2;
+    H_illum = abs(H_asym(-sfIdx,sfCutoffi)).^2;
 else
-    H_illum = abs(H(-d.sfIdx,p.sfCutoffi)).^2;
+    H_illum = abs(H(-sfIdx,sfCutoffi)).^2;
 end
-G1 = G(d.sfIdx,p.k,-d.zPosIdx);
-D1 = D(d.sfIdx,p.k,d.zPosIdx);
+G1 = G(sfIdx,k,-zPosIdx);
+D1 = D(sfIdx,k,zPosIdx);
 
 % Convolve effective partially coherent transmission CSFs in the frequency domain
 p1 = H_det.*G1.*H_illum;
@@ -16,7 +16,7 @@ p2 = conj(H_det).*D1;
 o1 = ifft2(fft2(p1).*fft2(p2));
 
 p1 = H_det.*conj(D1);
-p2 = conj(H_det).*conj(G1).*rot90(H_illum,2);
+p2 = conj(H_det).*conj(G1).*circshift(rot90(H_illum,2),[1 1]);
 o2 = ifft2(fft2(p1).*fft2(p2));
 
 out = o1 - o2; % add for phase OTF
